246fb352cbe870c0f0aba34434b64d68b36e9cf4
timmythecat <wjv8@pitt.edu>
2025-04-09 20:10:06 -0400

    Update MainPage.xaml.cs

--- HelpMyMom-PROJ/momUI/MainPage.xaml.cs
+++ HelpMyMom-PROJ/momUI/MainPage.xaml.cs
@@ -6,7 +6,8 @@ namespace momUI
 {
     public partial class MainPage : ContentPage
     {
-        string URL = $"http://localhost:5124/api";
+        //string URL = $"http://localhost:5124/api";
+        string URL = $"https://momapi20250409124316-bqevbcgrd7begjhy.canadacentral-01.azurewebsites.net/api";
    
 
         public MainPage()
