f36de82b1cfc74e005aea3fda9d3fcd6134cbb6a
timmythecat <wjv8@pitt.edu>
2025-04-11 11:17:36 -0400

    Create EmailServices.cs

---
+++ HelpMyMom-PROJ/momUI/EmailServices.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace momUI
+{
+    public static class EmailServices
+    {
+        // Valid statuses: new, assigned, in progress, completed, approved 
+        // Ticket is temporary until there is a ticket object
+        public static void SendNotifcation(String Target, String status, int ticket)
+        {
+            string fromEmail = "gingervip66@gmail.com";
+            string password = "udyw uyyi lsne kjrt";
+
+            string[] msgBody = new string[]
+            {
+                "Hello [USER]!",
+                $"Your ticket (#{ticket}) have now been updated to the {status} status, log into the app for additional details.",
+                "Thank you for your time, Help My Mom App"
+            };
+
+            MailAddress from = new MailAddress(fromEmail);
+            MailAddress to = new MailAddress(Target);
+            MailMessage message = new MailMessage(from, to);
+            message.Subject = "Ticket #" + ticket + "has been updated to " + status;
+            message.Body = string.Join("\n", msgBody);
+
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = new NetworkCredential(fromEmail, password);
+            smtpClient.EnableSsl = true;
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+            try
+            {
+                smtpClient.Send(message);
+                Console.WriteLine("Email sent successfully.");
+            }
+            catch (SmtpFailedRecipientsException ex)
+            {
+                Console.WriteLine("Error sending email: " + ex.Message);
+            }
+        }
+    }
+}
