f735a0bb32d845e7496ddbbb6962c9e3d9f524e6
Grout Gobbler <yad49@pitt.edu>
2025-04-15 19:21:04 -0400

    Finishing three big sections.
    DONE:
     - OpenProfile.
     - EditProfile.
     - ShowReviews.

--- HelpMyMom-PROJ/momUI/HelperViews/HelperEditProfile.xaml.cs
+++ HelpMyMom-PROJ/momUI/HelperViews/HelperEditProfile.xaml.cs
@@ -9,13 +9,14 @@ public partial class HelperEditProfile : ContentPage
 {
     String URL = "https://momapi20250409124316-bqevbcgrd7begjhy.canadacentral-01.azurewebsites.net/api";
     String MASTERusername = "UncleBensBiggestFan";
-    String newUsername = null, newFirstName = null, newLastName = null, newDescription = null;
+    String newUsername = null, newFirstName = null, newLastName = null, newDescription = null, newSpecsList = null, newDay, newMonth, newYear;
 
 	public HelperEditProfile()
 	{
 		InitializeComponent();
 	}
 
+    // USERNAME.
     private void UsernameEntry_TextChanged(object sender, TextChangedEventArgs e)
     {
         newUsername = e.NewTextValue;
@@ -91,6 +92,7 @@ public partial class HelperEditProfile : ContentPage
         newUsername = null;
     }
 
+    // FIRST NAME.
     private void FirstNameEntry_TextChanged(object sender, TextChangedEventArgs e)
     {
         newFirstName = e.NewTextValue;
@@ -172,7 +174,7 @@ public partial class HelperEditProfile : ContentPage
         newFirstName = null;
     }
 
-
+    // LAST NAME.
     private void LastNameEntry_TextChanged(object sender, TextChangedEventArgs e)
     {
         newLastName = e.NewTextValue;
@@ -253,7 +255,189 @@ public partial class HelperEditProfile : ContentPage
 
         newLastName = null;
     }
+    
+    // DATE OF BIRTH.
+    private void DateOfBirthMONTHEntry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        newMonth = e.NewTextValue;
+    }
 
+    private void DateOfBirthDAYEntry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        newDay = e.NewTextValue;
+    }
+
+    private void DateOfBirthYEAREntry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        newYear = e.NewTextValue;
+    }
+
+    async private void DateOfBirthButton_Clicked(object sender, EventArgs e)
+    {
+        using(HttpClient client = new HttpClient())
+        {
+            try
+            {
+                HttpResponseMessage response1 = await client.GetAsync($"{URL}/{"Accounts"}");
+                    String json1 = await response1.Content.ReadAsStringAsync();
+                    List<Account> listAccounts = JsonConvert.DeserializeObject<List<Account>>(json1); // listAccounts.
+                HttpResponseMessage response2 = await client.GetAsync($"{URL}/{"Helpers"}");
+                    String json2 = await response2.Content.ReadAsStringAsync();
+                    List<Helper> listHelpers = JsonConvert.DeserializeObject<List<Helper>>(json2); // listHelpers.
+
+                if (response1.IsSuccessStatusCode && response2.IsSuccessStatusCode)
+                {
+                    string accountUsername = MASTERusername;
+                    Helper tempHelper = new Helper();
+                    DateOnly fullDateOfBirth = new DateOnly();
+
+                    int? helperID = -1;
+                    foreach(Account a in listAccounts) // get helper ID from account.
+                    {
+                        if(a.Username == accountUsername)
+                        {
+                            helperID = a.HelperId;
+                            break;
+                        }
+                    }
+
+                    if(helperID == -1)
+                    {
+                        await DisplayAlert("AccountNotFoundError", "ERROR! Account not found.", "OK");
+                        return;
+                    }
+
+                    bool found = false;
+                    foreach(Helper h in listHelpers) // get helper object from helper ID.
+                    {
+                        if(h.Id == helperID)
+                        {
+                            tempHelper = h;
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (!found)
+                    {
+                        await DisplayAlert("HelperNotFoundError", "ERROR! Helper not found.", "OK");
+                        return;
+                    }
+
+                    fullDateOfBirth = new DateOnly(Int32.Parse(newYear), Int32.Parse(newMonth), Int32.Parse(newDay)); // Concatanate date of birth into one string.
+                    tempHelper.Dob = fullDateOfBirth;
+
+                    HttpResponseMessage response3 = await client.PutAsJsonAsync($"{URL}/Helpers/{helperID}", tempHelper); // replace helper.
+
+                    if (response3.IsSuccessStatusCode)
+                    {
+                        DateOfBirthButton.Text = "Success.";
+                    }
+                    else
+                    {
+                        DateOfBirthButton.Text = "Failure.";
+                    }
+                }
+                else
+                {
+                    DateOfBirthButton.Text = "Failed to access database.";
+                }
+            }
+            catch(Exception except)
+            {
+                DateOfBirthButton.Text = $"Exception occured ... {except}";
+            }
+        }
+
+        newDay = null;
+        newMonth = null;
+        newYear = null;
+    }
+
+    // SPECS.
+    private void SpecsEntry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        newSpecsList = e.NewTextValue;
+    }
+
+    async private void SpecsButton_Clicked(object sender, EventArgs e)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                HttpResponseMessage response1 = await client.GetAsync($"{URL}/{"Accounts"}");
+                    String json1 = await response1.Content.ReadAsStringAsync();
+                    List<Account> listAccounts = JsonConvert.DeserializeObject<List<Account>>(json1); // listAccounts.
+                HttpResponseMessage response2 = await client.GetAsync($"{URL}/{"Helpers"}");
+                    String json2 = await response2.Content.ReadAsStringAsync();
+                    List<Helper> listHelpers = JsonConvert.DeserializeObject<List<Helper>>(json2); // listHelpers.
+
+                if (response1.IsSuccessStatusCode && response2.IsSuccessStatusCode)
+                {
+                    String accountUsername = MASTERusername;
+                    Helper tempHelper = new Helper();
+
+                    int? helperID = -1;
+                    foreach (Account a in listAccounts) // find helper ID from Account.
+                    {
+                        if (a.Username == accountUsername)
+                        {
+                            helperID = a.HelperId;
+                            break;
+                        }
+                    }
+
+                    if (helperID == -1)
+                    {
+                        await DisplayAlert("AccountNotFoundError", "ERROR! Account not found.", "OK");
+                        return;
+                    }
+
+                    bool found = false;
+                    foreach (Helper h in listHelpers) // find Helper object from Helper ID.
+                    {
+                        if (h.Id == helperID)
+                        {
+                            found = true;
+                            tempHelper = h;
+                            break;
+                        }
+                    }
+
+                    if (!found)
+                    {
+                        await DisplayAlert("HelperNotFoundError", "ERROR! Helper not found.", "OK");
+                        return;
+                    }
+
+                    tempHelper.Specs = newSpecsList;
+                    HttpResponseMessage response3 = await client.PutAsJsonAsync($"{URL}/Helpers/{helperID}", tempHelper); // replace helper.
+
+                    if (response3.IsSuccessStatusCode)
+                    {
+                        SpecsButton.Text = "Success!";
+                    }
+                    else
+                    {
+                        SpecsButton.Text = "Failure!";
+                    }
+                }
+                else
+                {
+                    SpecsButton.Text = "Failed to access one of the database tables.";
+                }
+            }
+            catch (Exception except)
+            {
+                SpecsButton.Text = $"Exception occured ... {except}";
+            }
+        }
+
+        newSpecsList = null;
+    }
+
+    // DESCRIPTION.
     private void DescriptionEntry_TextChanged(object sender, TextChangedEventArgs e)
     {
         newDescription = e.NewTextValue;
--- HelpMyMom-PROJ/momUI/HelperViews/ViewReviews.xaml.cs
+++ HelpMyMom-PROJ/momUI/HelperViews/ViewReviews.xaml.cs
@@ -1,9 +1,53 @@
+using momUI.models;
+using Newtonsoft.Json;
+
 namespace momUI.HelperViews;
 
 public partial class ViewReviews : ContentPage
 {
+    String URL = "https://momapi20250409124316-bqevbcgrd7begjhy.canadacentral-01.azurewebsites.net/api";
+    List<ReviewView> listOfAllReviews;
+    int helperID = 1; // hard-coded helper ID.
+
+	private class ReviewView
+	{
+		public double numberOfStars { get; set; }
+        public String? reviewTextContent { get; set; }
+	}
+
 	public ViewReviews()
 	{
 		InitializeComponent();
-	}
+		listOfAllReviews = new List<ReviewView>();
+    }
+
+    protected override async void OnAppearing() // determines what the page does when it opens.
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync($"{URL}/{"Reviews"}");
+                String json = await response.Content.ReadAsStringAsync();
+                List<Review> reviewsList = JsonConvert.DeserializeObject<List<Review>>(json); // reviewsList.
+
+                    foreach (Review r in reviewsList)
+                    {
+                        if (r.HelperId == helperID)
+                        {
+                            ReviewView tempReviewView = new ReviewView();
+                            tempReviewView.reviewTextContent = r.Text;
+                            tempReviewView.numberOfStars = (double)(r.Stars / 2.0);
+                            listOfAllReviews.Add(tempReviewView);
+                        }
+                    }
+
+                AllReviewsListView.ItemsSource = listOfAllReviews;
+            }
+            catch (Exception e)
+            {
+                AllReviewsListView.ItemsSource = $"Exception occured! {e}";
+            }
+        }
+    }
 }
